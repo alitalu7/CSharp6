cd175303fd39dadf024a8f1af255495d0aeb4cfc
alitalu7 <55026802+alitalu7@users.noreply.github.com>
2022-06-23 23:18:55 +0300

    Add project files.

---
+++ CSharp6/Program.cs
@@ -0,0 +1,39 @@
+﻿
+Console.WriteLine("1 ile 7 arasında sayı giriniz...");
+int sayi = Convert.ToInt32(Console.ReadLine());
+
+switch (sayi)
+{
+    case 1:
+        Console.WriteLine("Pazartesi");
+        break;
+
+    case 2:
+        Console.WriteLine("Salı");
+        break;
+
+    case 3:
+        Console.WriteLine("Çarşamba");
+        break;
+
+    case 4:
+        Console.WriteLine("Perşembe");
+        break ;
+
+    case 5:
+        Console.WriteLine("Cuma");
+        break;
+
+    case 6:
+        Console.WriteLine("Cumartesi");
+        break;
+
+    case 7:
+        Console.WriteLine("Pazar");
+        break;
+
+    default:
+        Console.WriteLine("Lütfen 1 ile 7 arasında bir sayı giriniz...");
+        break;
+
+}
